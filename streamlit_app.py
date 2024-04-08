a75b16ba93e66c968c070b0f1c74511f301d15ba
elinevereyes <110269546+elinevereyes@users.noreply.github.com>
2024-04-08 14:01:35 -0400

    Update streamlit_app.py
    Cambio de titulo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="Elin Eve - AI")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
+    st.title('Elin Eve - AI')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
